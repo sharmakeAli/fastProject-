049afe20964116b2081d04beee5d4dc212a97da0
sharmakekahiye <kahiecode@gmail.com>
2024-03-16 01:02:22 +0300

    add book

--- src/main/java/com/example/fastProject/books/Book.java
+++ src/main/java/com/example/fastProject/books/Book.java
@@ -10,4 +10,25 @@ public class Book {
     private Long id;
     private String title;
     private String author;
+
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setAuthor(String author) {
+//        this.author = author;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
 }
